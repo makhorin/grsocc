0a4f2ea3abf17c162f5c1bbf1ca2678e3b9865c5
makhorin <andreymaxorin@gmail.com>
2018-05-25 03:27:56 +0300

    use different sdk

---
+++ Gravity Soccer/Assets/Scripts/PlayerMoveToX.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerMoveToX : MonoBehaviour
+    {
+        BallController _ball;
+        private void Start()
+        {
+                        
+        }
+
+        public void Init(BallController ball)
+        {
+            _ball = ball;
+        }
+
+        private void Update()
+        {
+            if (_ball == null)
+                return;
+            if (_ball.transform.position.y > transform.position.y)
+                return;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(_ball.transform.position.x, transform.position.y), Time.deltaTime);
+        }
+    }
+}
